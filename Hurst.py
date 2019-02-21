832cc1b72e6209125af8775063c4d85007231290
Aaron Elijah <aaronzakelijah@googlemail.com>
2019-02-21 16:40:41 +0800

    Hurst using Pandas

--- Hurst.py
+++ Hurst.py
@@ -7,32 +7,78 @@ Created on Thu Feb 14 16:06:32 2019
 """
 
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
 
-<<<<<<< HEAD
-with open("gemini_BTCUSD_2015_1min.csv") as csv_file:
+def openFile():
+    with open("gemini_BTCUSD_2015_1min.csv") as csv_file:
     
-    print("File opened!")
+        print("File opened!")
     
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    line_count = 0
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        line_count = 0
     
     
-  
-    
-    for row in csv_reader:
-        if line_count == 1:
-            #print(f'Column names are {", ".join(row)}')
-            line_count += 1
+        for row in csv_reader:
+            if line_count == 1:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+                
+            elif line_count == 0:
+                # skips over line_count = 0
+                # stops parsing over first line in .csv file
+                line_count += 1
             
-        elif line_count > 1:
-            #print(f'\t{row[0]} is timestamp, {row[1]} is date and {row[2]} is symbol.')
-            line_count += 1
+            elif line_count > 1:
+                #print(f'\t{row[0]} is timestamp, {row[1]} is date and {row[2]} is symbol.')
+                print(row)
+                line_count += 1
             
-        elif line_count == 0:
-            # skips over line_count = 0
-            # stops parsing over first line in .csv file
-            line_count += 1
-    print("Done!")
-=======
-file = open('gemini_BTCUSD_2015_1min.csv')
->>>>>>> 3788a6faec3f7df440e9ae825fcb903406f314bc
+            
+                
+        print("Done!")
+        
+def openFileAsPanda():
+    with open("gemini_BTCUSD_2015_1min.csv") as csv_file:
+    
+        print("File opened!")
+    
+        data = pd.read_csv(csv_file)
+    
+    return data
+            
+            
+                
+def main():
+    data = openFileAsPanda()
+    
+    #data['Close'].plot()
+    print(data.shape[0])
+    
+    volume = np.zeros(data.shape[0])
+    
+    #data['datetime'] = data['date'].map(lambda x: datetime.datetime.strptime(x, ))
+    data['Assets'] = data['Close'] * data['Volume']
+    data['Return'] = data['Close'].diff()
+    data.plot('Unix Timestamp', 'Return')
+    
+    
+    
+    """
+    for i in range(data.shape[0]):
+        volume[i] = data['Close'][i] * data['Volume'][i]
+
+    plt.plot(volume)
+    plt.show()
+    """
+    
+main()   
+    
+    
+    
+    
+    
+    
+    
