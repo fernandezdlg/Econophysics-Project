5f570f46071d19e3395c8268a4d444c3386e8ad3
Juan Fernández <a01196685@itesm.mx>
2019-03-01 14:14:38 +0800

    delete first lines comments

--- Hurst.py
+++ Hurst.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 14 16:06:32 2019
-
-@author: juanfernandez
-"""
-
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
