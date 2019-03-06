f52b26b08a88ff64506cc2a266b8879dae90e548
Juan Fernández <a01196685@itesm.mx>
2019-03-06 16:10:24 +0800

    Needs adaptation of code from jupyter notebook to our data arrays

--- Hurst.py
+++ Hurst.py
@@ -47,7 +47,14 @@ def openFileAsPanda():
 # returns the factors of a number n
 def factors(n):    
     return set(reduce(list.__add__, # this part adds each factor to a list
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))        
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))     
+
+def factors2(n,a): # returns a factors from number n
+    multiplier = n**(1/a)
+    factors = np.zeros(a)
+    for i in range(a):
+        factors[i] = np.int(np.floor(multiplier**(i+1)))
+    return factors
             
                 
 def main():
@@ -68,6 +75,18 @@ def main():
     length_max = len(data.index)
     fa = np.array(factors(length_max)) # Hope that length is not a prime number
     print(fa)
+#==============================================================================
+#     Instead of getting factors of the number, we could plan to get a certain
+#     number of factors for the Hurst plot and the round off to integers as
+#     showed below:
+#==============================================================================
+    fa2 = factors2(length_max,10)
+    print(fa2)
+    print('sample start')
+    for i in range(10):
+        print(np.zeros(np.int(fa2[i])))
+        
+    print('sample ends')
     
     plt.close()
     #data.plot('Formatted Date', 'Assets')
@@ -76,8 +95,22 @@ def main():
     # Find factors of the total length of list - 121580
     # Use this as block length to calculate Hurst exponent 
     
+    rav = []
+    for n in np.int(fa2):
+        r = []
+        for k in range(int(length_max/n)):
+            S = np.std(x[k*n:(k+1)*n])
+            R = max(X[k*n:(k+1)*n]) - min(X[k*n:(k+1)*n])
+            r.append(R/S)
+        rav.append(np.mean(r))
+        
+    lnN = np.log(np.int(fa2))
+    lnrav = np.log(rav)
     
-    
+    plnNlnrav = np.polyfit(lnN,lnrav,1)
+    lnravfit = np.polyval(plnNlnrav,lnN)
+
+
     
     """
     for i in range(data.shape[0]):
